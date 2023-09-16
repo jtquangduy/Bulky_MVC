ee80c316e5d91e8fa0c8bf4f08bd1c67564d5f25
Quang Duy <justinquangduy@gmail.com>
2023-09-16 17:23:16 +0700

    Server Side Validations

--- BulkyWeb/Models/Category.cs
+++ BulkyWeb/Models/Category.cs
@@ -10,10 +10,12 @@ namespace BulkyWeb.Models
         public int Id { get; set; }
 
         [Required]
+        [StringLength(30)]
         [DisplayName("Category Name")]
         public string Name { get; set; }
 
         [DisplayName("Display Order")]
+        [Range(1,100,ErrorMessage ="Display Order must be between 1-100")]
         public int DisplayOrder { get; set; }
     }
 }
