3a7e173b88bbae69912d44b4fbfc8d35395a30ec
Quang Duy <justinquangduy@gmail.com>
2023-09-18 17:49:45 +0700

    Create Category Post Handler

--- BulkyWebRazor_Temp/Pages/Categories/Create.cshtml.cs
+++ BulkyWebRazor_Temp/Pages/Categories/Create.cshtml.cs
@@ -5,11 +5,10 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace BulkyWebRazor_Temp.Pages.Categories
 {
+    [BindProperties]
     public class CreateModel : PageModel
     {
-
         private readonly ApplicationDbContext _db;
-
         public Category Category { get; set; }
 
         public CreateModel(ApplicationDbContext db)
@@ -20,6 +19,13 @@ namespace BulkyWebRazor_Temp.Pages.Categories
         public void OnGet()
         {
 
+        }
+
+        public IActionResult OnPost()
+        {
+            _db.Categories.Add(Category);
+            _db.SaveChanges();
+            return RedirectToPage("Index");
         }
     }
 }
