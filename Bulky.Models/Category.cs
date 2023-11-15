47cf0caf8e0f33695cf38175cc9d3e4c063e4ec5
Quang Duy <justinquangduy@gmail.com>
2023-11-15 17:00:47 +0700

    N-Tier Architecture

--- Bulky.Models/Category.cs
+++ Bulky.Models/Category.cs
@@ -1,8 +1,7 @@
-﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
-using System.ComponentModel;
+﻿using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 
-namespace BulkyWeb.Models
+namespace Bulky.Models
 {
     public class Category
     {
@@ -15,7 +14,7 @@ namespace BulkyWeb.Models
         public string Name { get; set; }
 
         [DisplayName("Display Order")]
-        [Range(1,100,ErrorMessage ="Display Order must be between 1-100")]
+        [Range(1, 100, ErrorMessage = "Display Order must be between 1-100")]
         public int DisplayOrder { get; set; }
     }
 }
