56904584d365a97513c08fc2a2b6db25d4fcd28d
Quang Duy <justinquangduy@gmail.com>
2023-11-21 16:04:28 +0700

    Implement ICategoryRepository

--- Bulky.DataAccess/Repository/IRepository/IRepository.cs
+++ Bulky.DataAccess/Repository/IRepository/IRepository.cs
@@ -1,24 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Linq.Expressions;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Linq.Expressions;
 
 namespace Bulky.DataAccess.Repository.IRepository
 {
-    internal interface IRepository<T> where T : class
+    public interface IRepository<T> where T : class
     {
         // T - Category
-
         IEnumerable<T> GetAll();
 
         T Get(Expression<Func<T, bool>> filter);
 
         void Add(T entity);
+
         void Remove(T entity);
+
         void RemoveRange(IEnumerable<T> entity);
-
-
     }
 }
