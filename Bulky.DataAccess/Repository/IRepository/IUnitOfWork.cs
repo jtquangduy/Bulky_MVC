19c2605eac418942f2ab04e96a405e4d9316a7e4
Quang Duy <justinquangduy@gmail.com>
2023-12-07 16:25:55 +0700

    Add Shopping Cart to Repository

--- Bulky.DataAccess/Repository/IRepository/IUnitOfWork.cs
+++ Bulky.DataAccess/Repository/IRepository/IUnitOfWork.cs
@@ -8,6 +8,8 @@
 
         ICompanyRepository Company { get; }
 
+        IShoppingCartRepository ShoppingCart { get; }
+
         void Save();
     }
 }
--- Bulky.DataAccess/Repository/UnitOfWork.cs
+++ Bulky.DataAccess/Repository/UnitOfWork.cs
@@ -12,12 +12,15 @@ namespace BulkyBook.DataAccess.Repository
 
         public ICompanyRepository Company { get; private set; }
 
+        public IShoppingCartRepository ShoppingCart { get; private set; }
+
         public UnitOfWork(ApplicationDbContext db)
         {
             _db = db;
             Category = new CategoryRepository(_db);
             Product = new ProductRepository(_db);
             Company = new CompanyRepository(_db);
+            ShoppingCart = new ShoppingCartRepository(_db);
         }
 
         public void Save()
