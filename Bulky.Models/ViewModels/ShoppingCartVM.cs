fd3fe6be7869e5621639f3d8ed321ab49bcbb525
Quang Duy <justinquangduy@gmail.com>
2023-12-11 15:59:56 +0700

    Make ShoppingCartVM more Dynamic

--- Bulky.Models/ViewModels/ShoppingCartVM.cs
+++ Bulky.Models/ViewModels/ShoppingCartVM.cs
@@ -1,15 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace BulkyBook.Models.ViewModels
+﻿namespace BulkyBook.Models.ViewModels
 {
     public class ShoppingCartVM
     {
         public IEnumerable<ShoppingCart> ShoppingCartList { get; set; }
-
-        public double OrderTotal { get; set; }
+        public OrderHeader OrderHeader { get; set; }
     }
 }
