68a4e8aa49f1f2ecb102e65e9ddd47d88892e4e6
Quang Duy <justinquangduy@gmail.com>
2023-12-14 15:01:07 +0700

    OrderVM and Order Controller

---
+++ Bulky.Models/ViewModels/OrderVM.cs
@@ -0,0 +1,9 @@
+﻿namespace BulkyBook.Models.ViewModels
+{
+    public class OrderVM
+    {
+        public OrderHeader OrderHeader { get; set; }
+
+        public IEnumerable<OrderDetail> OrderDetails { get; set; }
+    }
+}
